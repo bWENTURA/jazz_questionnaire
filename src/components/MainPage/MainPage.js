8fcf1bad42d18a547ec9676030f4354ed2771371
PIvSky <piotr.iwansky@gmail.com>
2023-03-05 18:32:11 +0100

    added basic elements to MainPage component

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './MainPage.css';
+
+const MainPage = () => {
+
+    return (
+        <article className="intro">
+            <div className="intro-container">
+                <h1 className="intro-container__question">Do You have predispositions <br></br> to be a jazzman?</h1>
+                <button className="intro-container__button ">Let's check it!</button>
+            </div>
+        </article>
+    )
+}
+
+export default MainPage;
