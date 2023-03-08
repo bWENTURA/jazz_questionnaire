9c239a98ad2da246db81fc81ae2cb67cff4a0f13
PIvSky <piotr.iwansky@gmail.com>
2023-03-08 11:05:13 +0100

    completed basics of MainPage component

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,15 +1,24 @@
 import React from "react";
 import './MainPage.css';
 
+import GithubIcon from "../icons/git-hub-icon.png"
+
 const MainPage = () => {
 
     return (
-        <article className="intro">
-            <div className="intro-container">
-                <h1 className="intro-container__question">Do You have predispositions <br></br> to be a jazzman?</h1>
-                <button className="intro-container__button ">Let's check it!</button>
-            </div>
-        </article>
+        <div>
+            <article className="intro">
+                <div className="corner-github">
+                    <a href="https://github.com/PIvSky" className="corner-github__icon">
+                        <img alt="remote-repo-icon" src={GithubIcon} className="corner-github__iconPic"/>
+                    </a>
+                </div>
+                <div className="intro-container">
+                    <h1 className="intro-container__question">Do you have predispositions <br></br> to be a jazzman?</h1>
+                    <button className="intro-container__button ">Let's check it!</button>
+                </div>
+            </article>
+        </div>
     )
 }
 
