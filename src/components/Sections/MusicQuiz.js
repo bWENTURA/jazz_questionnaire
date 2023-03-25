16fe467494dc9c5a56b61bc2504ee6dbca269c36
PIvSky <piotr.iwansky@gmail.com>
2023-03-25 19:40:33 +0100

    some styling changes done in Quiz and UserData components

--- src/components/Sections/MusicQuiz.js
+++ src/components/Sections/MusicQuiz.js
@@ -1,4 +1,4 @@
-import {React, Fragment, useState} from "react";
+import {React, Fragment, useState, useRef} from "react";
 import Player from "./Player";
 import {MdExpandMore, MdExpandLess} from 'react-icons/md';
 import './MusicQuiz.css';
@@ -6,9 +6,9 @@ import './MusicQuiz.css';
 const MusicQuiz = () => {
 
     const [expand, setExpand] = useState(false);
-    // const [songs, setSongs] = useState(songsData);
-    // const [isPlaying, setIsPlaying] = useState(false);
-    // const [currentSong, setCurrentSong] = useState(songsData[0]);
+    const [songs, setSongs] = useState(songsData);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentSong, setCurrentSong] = useState(songsData[0]);
 
     // changin visibility of section
     const expandMoreHandler = () => {
@@ -19,39 +19,40 @@ const MusicQuiz = () => {
         setExpand(true)
     };
 
-    // const songsData = [
-    //     {
-    //         title: "Beirut - Prezlaurberg",
-    //         src: '../audio/Prenzlaurberg.mp3',
-    //         id: 0,
-    //     },
-    //     {
-    //         title: "Art Blakey - Moanin",
-    //         src: "../audio/Moanin'.mp3",
-    //         id: 1,
-    //     },
-    //     {
-    //         title: "Bill Evans - Nardis",
-    //         src: "../audio/Nardis.mp3",
-    //         id: 2,
-    //     },
-    //     {
-    //         title: "Bob Marley - No Woman No Cry",
-    //         src: "../audio/No-Woman-No-Cry.mp3",
-    //         id: 3,
-    //     },
-    //     {
-    //         title: "Brad Mehldau - Paris",
-    //         src: "../audio/Paris.mp3",
-    //         id: 4,
-    //     },
-    //     {
-    //         title: "Arms and the sleepers",
-    //         src: "../audio/The-Architekt.mp3",
-    //         id: 5,
-    //     },
-    // ]
+    const songsData = [
+        {
+            title: "Beirut - Prezlaurberg",
+            src: '../audio/Prenzlaurberg.mp3',
+            id: 0,
+        },
+        {
+            title: "Art Blakey - Moanin",
+            src: "../audio/Moanin'.mp3",
+            id: 1,
+        },
+        {
+            title: "Bill Evans - Nardis",
+            src: "../audio/Nardis.mp3",
+            id: 2,
+        },
+        {
+            title: "Bob Marley - No Woman No Cry",
+            src: "../audio/No-Woman-No-Cry.mp3",
+            id: 3,
+        },
+        {
+            title: "Brad Mehldau - Paris",
+            src: "../audio/Paris.mp3",
+            id: 4,
+        },
+        {
+            title: "Arms and the sleepers",
+            src: "../audio/The-Architekt.mp3",
+            id: 5,
+        },
+    ]
 
+    //  
 
     return (
         <Fragment>
